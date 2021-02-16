183fa4d923095a6b896e671870dee23014e6b5f8
voronik1801 <divi1801@icloud.com>
2021-02-16 09:55:26 +0700

    init commit

---
+++ MethodOptimization/Function.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodOptimization
+{
+    class DichotomyMethod
+    {
+        public delegate double Function(double x);
+
+        public double FindMin(Function function, double error)
+        {
+            return 0.0d;//TODO
+        }
+    }
+}
