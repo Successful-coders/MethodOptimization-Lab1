cfff523d333eccc84c580f18c09203f6f760b17d
Voronik <divi1801@icloud.com>
2021-02-28 15:10:58 +0700

    gold & dichotomy

--- FindMethodsOned/main.py
+++ FindMethodsOned/main.py
@@ -3,6 +3,7 @@
 import numpy as np
 def f(x):
     return (x-8)**2
+
 class Dichotomy:
     def x01(self,a,b):
         return (a+b-0.0001)/2
@@ -11,13 +12,79 @@ class Dichotomy:
         return (a+b+0.0001)/2
 
     def DihotomyMethod(self,a,b):
-        x1 = self.x01(a,b)
-        x2 = self.x02(a, b)
-        print (x1)
-        print(x2)
+        i=1
+        while(abs(b-a)> 0.001):
+            print ("i:",i)
+            print("a",a)
+            print("b",b)
+            print("b-a",b-a)
+            x1 = self.x01(a, b)
+            print ("x1:",x1)
+            x2 = self.x02(a, b)
+            print("x2:",x2)
+            print("f(x1):", f(x1))
+            print("f(x2):", f(x2))
+            if(f(x1)<f(x2)):
+                b = x2
+            else:
+                a=x1
+            i=i+1
+            print("\n")
+
+class GoldenRatio:
+    def x01(self,a,b):
+        return a+0.381966011*(b-a)
+
+    def x02(self,a,b):
+        return a+0.618003399*(b-a)
+    def GoldenRatioMethod(self, a,b):
+        i = 1
+        while(abs(b-a)>0.001):
+            print("i:", i)
+            print("a", a)
+            print("b", b)
+            print("b-a", b - a)
+            x1 = self.x01(a, b)
+            x2 = self.x02(a, b)
+            print("x1:", x1)
+            print("x2:", x2)
+            print("f(x1):", f(x1))
+            print("f(x2):", f(x2))
+            if(f(x1)<f(x2)):
+                b = x2
+                x2 = x1
+            else:
+                a = x1
+                x1 = x2
+            i = i + 1
+            print("\n")
+
+class Fibonachi:
+    def x01(self,a,b,n):
+        return a + (self.Fib(n+1) / self.Fib(n+3))*(b-a)
+
+    def x02(self,a,b,n):
+        return a + (self.Fib(n+2) / self.Fib(n+3))*(b-a)
+
+    def Fib(selfself, n):
+        if n < 3:
+            return 1
+        return selfself.Fib(n - 1) + selfself.Fib(n - 2)
+
+    def FibonachiMethod(self, a,b):
+        i = 0
+
+
 
 
 a: float = -2
 b: float = 20
+print("Dichotomy method:")
 dichotomy = Dichotomy()
 dichotomy.DihotomyMethod(a,b)
+
+goldenRatio = GoldenRatio()
+goldenRatio.GoldenRatioMethod(a,b)
+
+fibonachi = Fibonachi()
+fibonachi.FibonachiMethod(a,b)
